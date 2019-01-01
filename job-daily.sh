461b7c174f487793cc7e2a551d88c82406591f9f
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2018-12-31 22:16:25 -0600

    Add download of correlation as html table to daily script

--- job-daily.sh
+++ job-daily.sh
@@ -2,8 +2,10 @@
 DATE=`date +%Y-%m-%d`
 curl -o correlation.png https://net-src.herokuapp.com/correlation.png
 curl -o correlation.csv https://net-src.herokuapp.com/correlation.csv
+curl -o correlation.txt https://net-src.herokuapp.com/correlation.txt
 curl -o rankings.csv https://net-src.herokuapp.com/ranking.csv
 
 mv correlation.png correlation_${DATE}.png
 mv correlation.csv correlation_${DATE}.csv
+mv correlation.txt correlation_${DATE}.txt
 mv rankings.csv rankings_${DATE}.csv
