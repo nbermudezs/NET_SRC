5b2921c6188f2e4dea0cd2fc5586c7bf9f9786ac
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2019-01-15 20:54:07 -0600

    Add outlier retrieval to daily cron job

--- job-daily.sh
+++ job-daily.sh
@@ -4,8 +4,10 @@ curl -o correlation.png https://net-src.herokuapp.com/correlation.png
 curl -o correlation.csv https://net-src.herokuapp.com/correlation.csv
 curl -o correlation.txt https://net-src.herokuapp.com/correlation.txt
 curl -o rankings.csv https://net-src.herokuapp.com/ranking.csv
+curl -o outliers.json https://net-src.herokuapp.com/outliers.json
 
 mv correlation.png correlation_${DATE}.png
 mv correlation.csv correlation_${DATE}.csv
 mv correlation.txt correlation_${DATE}.txt
 mv rankings.csv rankings_${DATE}.csv
+mv outliers.json outliers_${DATE}.json
