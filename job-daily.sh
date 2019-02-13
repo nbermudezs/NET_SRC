f1050f22bbc4b6a102bf8edf0bddbc0ebd3ef426
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2019-02-13 10:52:10 -0600

    Refactor daily cron job

--- job-daily.sh
+++ job-daily.sh
@@ -1,23 +1,26 @@
 #!/usr/bin/env bash
+
+export DATA_HOME='REPLACE_ME'
+export SENDGRID_TO_DEVELOPER='REPLACE_ME'
+export SENDGRID_FROM='REPLACE_ME'
+export SENDGRID_API_KEY='REPLACE_ME'
+
+curl -o ${DATA_HOME}/correlation.png https://net-src.herokuapp.com/correlation.png
+sleep 60
+
 DATE=`date +%Y-%m-%d`
-curl -o correlation.png https://net-src.herokuapp.com/correlation.png
-curl -o correlation_PowerSeven_${DATE}.png https://net-src.herokuapp.com/correlation.png?conf=PowerSeven
-curl -o correlation_Others_${DATE}.png https://net-src.herokuapp.com/correlation.png?conf=Others
-curl -o correlation.csv https://net-src.herokuapp.com/correlation.csv
-curl -o correlation_PowerSeven_${DATE}.csv https://net-src.herokuapp.com/correlation.csv?conf=PowerSeven
-curl -o correlation_Others_${DATE}.csv https://net-src.herokuapp.com/correlation.csv?conf=Others
-curl -o correlation.txt https://net-src.herokuapp.com/correlation.txt
-curl -o rankings.csv https://net-src.herokuapp.com/ranking.csv
-curl -o rankings_PowerSeven_${DATE}.csv https://net-src.herokuapp.com/ranking.csv?conf=PowerSeven
-curl -o rankings_Others_${DATE}.csv https://net-src.herokuapp.com/ranking.csv?conf=Others
-curl -o outliers.json https://net-src.herokuapp.com/outliers.json
-curl -o net-v-all.png https://net-src.herokuapp.com/net-v-all.png
+curl -o ${DATA_HOME}/correlation_${DATE}.png https://net-src.herokuapp.com/correlation.png
+curl -o ${DATA_HOME}/correlation_PowerSeven_${DATE}.png https://net-src.herokuapp.com/correlation.png?conf=PowerSeven
+curl -o ${DATA_HOME}/correlation_Others_${DATE}.png https://net-src.herokuapp.com/correlation.png?conf=Others
+curl -o ${DATA_HOME}/correlation_${DATE}.csv https://net-src.herokuapp.com/correlation.csv
+curl -o ${DATA_HOME}/correlation_PowerSeven_${DATE}.csv https://net-src.herokuapp.com/correlation.csv?conf=PowerSeven
+curl -o ${DATA_HOME}/correlation_Others_${DATE}.csv https://net-src.herokuapp.com/correlation.csv?conf=Others
+curl -o ${DATA_HOME}/correlation_${DATE}.txt https://net-src.herokuapp.com/correlation.txt
+curl -o ${DATA_HOME}/rankings_${DATE}.csv https://net-src.herokuapp.com/ranking.csv
+curl -o ${DATA_HOME}/rankings_PowerSeven_${DATE}.csv https://net-src.herokuapp.com/ranking.csv?conf=PowerSeven
+curl -o ${DATA_HOME}/rankings_Others_${DATE}.csv https://net-src.herokuapp.com/ranking.csv?conf=Others
+curl -o ${DATA_HOME}/outliers_${DATE}.json https://net-src.herokuapp.com/outliers.json
+curl -o ${DATA_HOME}/net-v-all.png https://net-src.herokuapp.com/net-v-all.png
 
-curl -o correlationByConf_A_${DATE}.png https://net-src.herokuapp.com/correlationByConf.png?n=8
-curl -o correlationByConf_B_${DATE}.png https://net-src.herokuapp.com/correlation.png?n=2
-
-mv correlation.png correlation_${DATE}.png
-mv correlation.csv correlation_${DATE}.csv
-mv correlation.txt correlation_${DATE}.txt
-mv rankings.csv rankings_${DATE}.csv
-mv outliers.json outliers_${DATE}.json
+cd ${NET_SRC_HOME}
+/usr/bin/python3 -m utils.error_notifier ${DATA_HOME}/rankings_${DATE}.csv
