058b33cd5e0a80220c02f54371c957f23375a01f
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2019-02-18 17:11:22 -0600

    Collect NET from NCAA website

--- templates/hello/collect_data.py
+++ templates/hello/collect_data.py
@@ -10,7 +10,7 @@ import pandas as pd
 import re
 import seaborn as sns
 import os
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 
 
@@ -32,6 +32,8 @@ espn_base_url = 'http://www.espn.com/mens-college-basketball/bpi/_/view/bpi/seas
 pomeroy_base_url = 'https://kenpom.com/index.php?y={}'
 
 
+ncaa_base_url = 'https://www.ncaa.com/rankings/basketball-men/d1/ncaa-mens-basketball-net-rankings'
+
 # template
 # pos 0: year
 sagarin_base_url = 'https://www.usatoday.com/sports/ncaab/sagarin/{}/team/'
@@ -50,7 +52,9 @@ def cleanup_warren_nolan_data(data):
 name_map = {
     'Alabama St.': 'Alabama State',
     'Albany-NY': 'Albany',
+    'Albany (NY)': 'Albany',
     'Alcorn St.': 'Alcorn State',
+    'Alcorn': 'Alcorn State',
     'American U.': 'American',
     'Appalachian St.': 'Appalachian State',
     'Arizona St.': 'Arizona State',
@@ -58,6 +62,7 @@ name_map = {
     'Ark.-Pine Bluff': 'Arkansas-Pine Bluff',
     'Arkansas St.': 'Arkansas State',
     'Army West Point': 'Army',
+    'A&M-Corpus Christi': 'Texas A&M-Corpus Christi',
     'Ball St.': 'Ball State',
     'Bethune Cookman': 'Bethune-Cookman',
     'Binghamton-NY': 'Binghamton',
@@ -76,19 +81,32 @@ name_map = {
     'Call State Northridge': 'Cal State Northridge',
     'Cal Baptist': 'California Baptist',
     'Cal Poly-SLO': 'Cal Poly',
+    'Central Ark.': 'Central Arkansas',
     'Central Connecticut St.': 'Central Connecticut',
     'Central Connecticut State': 'Central Connecticut',
+    'Central Conn. State': 'Central Connecticut',
     'Central Florida(UCF)': 'UCF',
+    'Central Mich.': 'Central Michigan',
+    'Charleston So.': 'Charleston Southern',
     'Chicago St.': 'Chicago State',
     'Cleveland St.': 'Cleveland State',
+    'Coastal Caro.': 'Coastal Carolina',
     'College of Charleston': 'Charleston',
+    'Col. of Charleston': 'Charleston',
     'Colorado St.': 'Colorado State',
     'Coppin St.': 'Coppin State',
+    'CSUN': 'Cal State Northridge',
     'Delaware St.': 'Delaware State',
     'Detroit Mercy': 'Detroit',
     'East Tennessee St.': 'East Tennessee State',
     'East Tennessee State(ETS': 'East Tennessee State',
+    'Eastern Ky.': 'Eastern Kentucky',
+    'Eastern Ill.': 'Eastern Illinois',
+    'Eastern Mich.': 'Eastern Michigan',
+    'Eastern Wash.': 'Eastern Washington',
+    'ETSU': 'East Tennessee State',
     'Florida Atlantic': 'FAU',
+    'Fla. Atlantic': 'FAU',
     'Florida Gulf Coast': 'FGCU',
     'Florida International': 'FIU',
     'Fla. International': 'FIU',
@@ -97,6 +115,7 @@ name_map = {
     'Fresno St.': 'Fresno State',
     'Gardner Webb': 'Gardner-Webb',
     'Georgia St.': 'Georgia State',
+    'Ga. Southern': 'Georgia Southern',
     'Grambling St.': 'Grambling',
     'Grambling State': 'Grambling',
     "Hawai'i": 'Hawaii',
@@ -105,6 +124,8 @@ name_map = {
     'Illinois-Chicago': 'UIC',
     'Iona College': 'Iona',
     'Kansas City(UMKC)': 'UMKC',
+    'La.-Monroe': 'ULM',
+    'Lamar University': 'Lamar',
     'LIU Brooklyn': 'Long Island',
     'Long Island U.(LIU)': 'Long Island',
     'Louisiana Monroe': 'ULM',
@@ -112,6 +133,7 @@ name_map = {
     'Loyola Marymount': 'Loyola-Marymount',
     'Loyola MD': 'Loyola-Maryland',
     'Loyola (MD)': 'Loyola-Maryland',
+    'Loyola Maryland': 'Loyola-Maryland',
     'MVSU(Miss. Valley State)': 'Mississippi Valley State',
     'McNeese': 'McNeese State',
     'Maryland-Eastern Shore': 'Maryland Eastern Shore',
@@ -122,8 +144,10 @@ name_map = {
     'Miami': 'Miami (FL)',
     'Miami-Ohio': 'Miami (OH)',
     'Miami OH': 'Miami (OH)',
+    'Middle Tenn.': 'Middle Tennessee',
     'Mississippi': 'Ole Miss',
     u'MVSU(Miss. Valley State)': 'Mississippi Valley State',
+    'Mississippi Val.': 'Mississippi Valley State',
     'Monmouth-NJ': 'Monmouth',
     u'Mount State Mary\'s': "Mount Saint Mary's",
     u'Mt. State Mary\'s': "Mount Saint Mary's",
@@ -133,13 +157,22 @@ name_map = {
     'NC Asheville': 'UNC Asheville',
     'NC Wilmington': 'UNCW',
     'NC Central': 'North Carolina Central',
+    'N.C. Central': 'North Carolina Central',
     'NC Greensboro': 'UNCG',
     'NC State': 'North Carolina State',
+    'N.C. State': 'North Carolina State',
+    'N.C. A&T': 'North Carolina A&T',
     'Nicholls': 'Nicholls State',
     'North Florida(UNF)': 'North Florida',
+    'North Ala.': 'North Alabama',
+    'Northern Ariz.': 'Northern Arizona',
+    'Northern Colo.': 'Northern Colorado',
+    'Northern Ky.': 'Northern Kentucky',
+    'Northern Ill.': 'Northern Illinois',
     'Oakland-Mich.': 'Oakland',
     'Omaha(Neb.-Omaha)': 'Omaha',
     'Pennsylvania': 'Penn',
+    'Prairie View': 'Prairie View A&M',
     'Presbyterian': 'Presbyterian College',
     'SC State': 'South Carolina State',
     'SE Missouri State(SEMO)': 'Southeast Missouri',
@@ -154,20 +187,30 @@ name_map = {
     'St. Francis-NY': 'Saint Francis (NY)',
     'St. Francis NY': 'Saint Francis (NY)',
     'St. Francis (BKN)': 'Saint Francis (NY)',
+    'St. Francis Brooklyn': 'Saint Francis (NY)',
     "Saint Joseph's-Pa.": "Saint Joseph's",
     "Saint Mary's-Cal.": "Saint Mary's College",
     "Saint Mary's": "Saint Mary's College",
+    "Saint Mary's (CA)": "Saint Mary's College",
     'Seattle': 'Seattle University',
+    'Seattle U': 'Seattle University',
     'SIU-Edwardsville': 'SIUE',
     'SIU Edwardsville': 'SIUE',
+    'South Fla.': 'South Florida',
     'Southern U.': 'Southern',
     'Southern California': 'USC',
+    'Southern Ill.': 'Southern Illinois',
+    'Southern Miss.': 'Southern Miss',
+    'Southeast Mo. State': 'Southeast Missouri',
     'Southeast Missouri State': 'Southeast Missouri',
     'Southeast Missouri St.': 'Southeast Missouri',
+    'Southeastern La.': 'Southeastern Louisiana',
     'SE Missouri St': 'Southeast Missouri',
     'SE Louisiana': 'Southeastern Louisiana',
+    'SFA': 'Stephen F. Austin',
     'Stony Brook-NY': 'Stony Brook',
     "St. John's": "Saint John's",
+    "St. John's (NY)": "Saint John's",
     'Texas San Antonio': 'UTSA',
     'Texas A&M-CorpusChristi': 'Texas A&M-Corpus Christi',
     'Texas A&M Corpus Chris': 'Texas A&M-Corpus Christi',
@@ -175,14 +218,20 @@ name_map = {
     'UConn': 'Connecticut',
     'UL Monroe': 'ULM',
     'UMass Lowell': 'UMass-Lowell',
+    'UMES': 'Maryland Eastern Shore',
     'UNC Greensboro': 'UNCG',
     'UNC Wilmington': 'UNCW',
+    'UNI': 'Northern Iowa',
     'USC Upstate': 'South Carolina Upstate',
     'UT Rio Grande Valley': 'UTRGV',
     'UT Arlington': 'UTA',
     'UT Martin': 'Tennessee-Martin',
     'Tennessee Martin': 'Tennessee-Martin',
     'VCU(Va. Commonwealth)': 'VCU',
+    'Western Ky.': 'Western Kentucky',
+    'Western Ill.': 'Western Illinois',
+    'Western Mich.': 'Western Michigan',
+    'Western Caro.': 'Western Carolina',
     'Xavier-Ohio': 'Xavier'
 }
 def cleanup_team_name(name):
@@ -216,12 +265,27 @@ def warren_nolan_html_to_df(year, metric, cols, col_offset=0):
     return df[cols].set_index('Team')
 
 
-def collect_from_warren_nolan():
-    metric = 'rpi-live'
-    net = warren_nolan_html_to_df(YEAR, 'net', ['Year', 'Team', 'NET Rank'], col_offset=1)
-    rpi = warren_nolan_html_to_df(YEAR, 'rpi-live', ['Year', 'Team', 'RPI'], col_offset=2)
+def collect_net(year):
+    req = Request(ncaa_base_url, headers={'User-Agent': 'Mozilla/5.0'})
+    results = []
+    print(ncaa_base_url)
+    soup = BeautifulSoup(urlopen(req), 'lxml')
+    table = soup.find('article', class_='rankings-content').find('table')
+    for row in table.find_all('tr'):
+        if row.find('th'):
+            continue
+        data = [col.get_text() for i, col in enumerate(row.find_all('td')) if i in [0, 2]]
+        data[1] = cleanup_team_name(data[1])
+        results.append([str(year)] + data)
+    df = pd.DataFrame.from_records(results, columns=['Year', 'NET Rank', 'Team'])
+    return df.set_index('Team')
 
-    return pd.concat([rpi, net], axis=1).T.drop_duplicates().T
+
+def collect_from_warren_nolan():
+    # net = warren_nolan_html_to_df(YEAR, 'net', ['Year', 'Team', 'NET Rank'], col_offset=1)
+    rpi = warren_nolan_html_to_df(YEAR, 'rpi-live', ['Year', 'Team', 'RPI'], col_offset=2)
+    return rpi
+    # return pd.concat([rpi, net], axis=1).T.drop_duplicates().T
 
 
 def collection_from_espn():
@@ -308,6 +372,8 @@ def collect_from_sagarin():
 
 def collect_rankings():
     import numpy as np
+    net_data = collect_net(YEAR)
+    print(net_data.shape, np.unique(net_data.index).shape)
     sagarin_data = collect_from_sagarin()
     print(sagarin_data.shape, np.unique(sagarin_data.index).shape)
     pomeroy_data = collect_from_pomeroy()
@@ -315,10 +381,10 @@ def collect_rankings():
     bpi_data = collection_from_espn()
     print(set(sagarin_data.index) - set(bpi_data.index))
     print(bpi_data.shape, np.unique(bpi_data.index).shape)
-    net_rpi_data = collect_from_warren_nolan()
-    print(net_rpi_data.shape, np.unique(net_rpi_data.index).shape)
+    rpi_data = collect_from_warren_nolan()
+    print(rpi_data.shape, np.unique(rpi_data.index).shape)
 
-    all_df = pd.concat([sagarin_data, pomeroy_data, bpi_data, net_rpi_data], axis=1)
+    all_df = pd.concat([sagarin_data, pomeroy_data, bpi_data, rpi_data, net_data], axis=1)
     all_df = all_df[['Year', 'Sagarin_RK', 'Pomeroy_RK', 'BPI_RK', 'RPI', 'NET Rank', 'Conf']]
     all_df = all_df.drop('Year', axis=1).astype({
         'Sagarin_RK': int,
