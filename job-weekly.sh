c4ea17328b9fc57a674e074a9c876b7d164f12a7
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2019-04-26 11:44:21 -0500

    Update weekly job

--- job-weekly.sh
+++ job-weekly.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-# make sure to set the NET_SRC_HOME env variable by doing
-# export NET_SRC_HOME=~/somepath
+# This script assumes that:
+# 1. the entire repository is available under the /home/bracketodds/NET_SRC directory
+# 2. there is a Python virtual environment under /home/bracketodds/python-venv
+# 3. the files summarizing the weekly data will be stored in /home/bracketodds/cron/net_src/
 
-# also set the env variable DAILY_DATA
-# i.e. export DAILY_DATA=/home/nbermudezs/cron/net_src/
+source /home/bracketodds/python-venv/bin/activate
+cd /home/bracketodds/NET_SRC
+python -m utils.trendline /home/bracketodds/cron/net_src/
+python -m utils.rpi_trendline /home/bracketodds/cron/net_src/
 
-cd ${NET_SRC_HOME}
-python -m utils.trendline ${DAILY_DATA}
+export DAILY_DATA='/home/bracketodds/cron/net_src/'
+export SENDGRID_TO='REPLACE_ME'
+export SENDGRID_FROM='REPLACE_ME'
+export SENDGRID_TEMPLATE_ID='REPLACE_ME'
+export SENDGRID_API_KEY='REPLACE_ME'
+python -m utils.send_email ${DAILY_DATA}
